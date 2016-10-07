72b3624da38b1941dd23bc1d532812f0dc8e6e32
JungleColder <oneou6688@163.comm>
2016-10-07 11:53:27 +0800

    test

---
+++ HelloMVC/Models/ProductDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class ProductDBContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ HelloMVC/Scripts/movies/movie_index_video.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wwtliu on 14/11/27.
+ */
+
+$(document).ready(function(){
+
+            $("#video2").attr("src", "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");//更新url
+            $("#video2").attr("autoplay", "true");//直接播放
+
+            $("#video3").attr("src", "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");//更新url
+            $("#video3").attr("autoplay", "true");//直接播放
+});
